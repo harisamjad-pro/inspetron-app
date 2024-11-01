732f1b9ac8d09fd8a85fdd23d037aec4df9ce640
Haris Amjad <harisamjad56@gmail.com>
2024-11-02 04:42:54 +0500

    tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,34 +1,47 @@
 "use client";
 
-import Link from 'next/link'
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React, { useState } from 'react'
+import React from 'react';
 import { GoHome } from "react-icons/go";
 import { IoSearchOutline } from "react-icons/io5";
+import { IoNotificationsOutline } from "react-icons/io5";
 
-const Sidebar = () => {
-    // const [nav, setNav] = useState([]);
-    const navs = [
+interface NavItem {
+    title: string;
+    icon: React.ReactNode;
+    href: string;
+}
+
+const Sidebar: React.FC = () => {
+    const navs: NavItem[] = [
         { title: "Home", icon: <GoHome className='size-6' />, href: "/" },
         { title: "Search", icon: <IoSearchOutline className='size-6' />, href: "/s" },
-        { title: "Notifications", icon: <GoHome className='size-6' />, href: "/d" },
+        { title: "Notifications", icon: <IoNotificationsOutline className='size-6' />, href: "/d" },
         { title: "Templates", icon: <GoHome className='size-6' />, href: "/s" },
-    ]
+    ];
 
-    console.log(usePathname())
+    const pathname = usePathname();
 
     return (
         <aside className='bg-[#000000]'>
-            <nav className='px-12 py-6'>
+            <nav className='p-6'>
                 <ul className='flex flex-col gap-4'>
-                    {navs?.map((nav) => (
-                        // eslint-disable-next-line react-hooks/rules-of-hooks
-                        <li key={nav.title}><Link href="/" className={`font-medium px-4 py-2 rounded-2xl flex items-center gap-2 group text-[#787878] hover:text-[#FFFFFF] hover:bg-[#242529] w-fit ${usePathname() === nav.href && "bg-[#242529] text-[#FFFFFF]"}`}>{nav.icon}{nav.title}</Link></li>
+                    {navs.map((nav) => (
+                        <li key={nav.title}>
+                            <Link
+                                href={nav.href}
+                                className={`font-medium px-4 py-2 rounded-2xl flex items-center gap-2 group text-[#787878] hover:text-[#FFFFFF] hover:bg-[#242529] w-fit ${pathname === nav.href ? "bg-[#242529] text-[#FFFFFF]" : ""}`}
+                            >
+                                {nav.icon}
+                                {nav.title}
+                            </Link>
+                        </li>
                     ))}
                 </ul>
             </nav>
         </aside>
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
